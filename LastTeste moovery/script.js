77e01224baee20528906be8b9a4f4b58919daf94
Levi Oliveira <123578597+LeviOliveir4@users.noreply.github.com>
2023-03-10 12:43:36 -0300

    Update script.js

--- LastTeste moovery/script.js
+++ LastTeste moovery/script.js
@@ -1,27 +1,27 @@
-function inverte(){
-    var num1 = parseFloat(document.getElementById("numero1").value);
-    var num2 = parseFloat(document.getElementById("numero2").value);
+// function inverte(){
+//     var num1 = parseFloat(document.getElementById("numero1").value);
+//     var num2 = parseFloat(document.getElementById("numero2").value);
 
 
-    var aux = num1;
+//     var aux = num1;
 
-    num1 = num2
-    num2 = aux;
+//     num1 = num2
+//     num2 = aux;
 
-    alert("Antes: \n" +
-         "numero1=" +document.getElementById("numero1").value+"\n"+
-         "numero2=" +document.getElementById("numero2").value+"\n \n"+
-         "Invertido:\n"+
-         "numero1=" +num1+"\n"+
-         "numero2=" +num2+"\n\n");
+//     alert("Antes: \n" +
+//          "numero1=" +document.getElementById("numero1").value+"\n"+
+//          "numero2=" +document.getElementById("numero2").value+"\n \n"+
+//          "Invertido:\n"+
+//          "numero1=" +num1+"\n"+
+//          "numero2=" +num2+"\n\n");
 
-}
+// }
 
 
 //Problema resolvido(Não da forma correta)!
-// let a = 1
-// let b = 2
+let a = 1
+let b = 2
 
-// console.log(`O valor de A: ${a} O valor de B: ${b}`);
+console.log(`O valor de A: ${a} O valor de B: ${b}`);
 
-// console.log(` Valores invertidos: valor de B: ${b - a} e o valor de A: ${ a + 1}`);
+console.log(` Valores invertidos: valor de B: ${b - a} e o valor de A: ${ a + 1}`);
